70ef811ca999d5e032af932453d081a6c0e2d94a
JEViz <jevizcaya77@gmail.com>
2025-04-23 05:57:25 +0200

    cambiar pass ok

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
     path('plantilla/', views.plantilla, name='plantilla'),
     path('jugador/<int:jugador_id>/', views.detalle_jugador, name='detalle_jugador'),
     path('partidos/', views.partidos, name='partidos'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='usuarios/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='usuarios/password_change_done.html'), name='password_change_done'),
 ]
 
 if settings.DEBUG:
